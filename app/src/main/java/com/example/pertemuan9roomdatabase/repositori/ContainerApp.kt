7a21a56c6c4fb7b0b0aabbf880d246edaf72b597
adilaroisa <adilaroisa688@gmail.com>
2025-11-28 13:33:02 +0700

    membuat file ContainerApp untuk menyiapkan database dan repository agar bisa dipakai oleh seluruh aplikasi lewat ViewModel

---
+++ app/src/main/java/com/example/pertemuan9roomdatabase/repositori/ContainerApp.kt
@@ -0,0 +1,30 @@
+package com.example.pertemuan9roomdatabase.repositori
+
+import android.app.Application
+import android.content.Context
+import com.example.pertemuan9roomdatabase.room.DatabaseSiswa
+
+interface ContainerApp {
+    val repositoriSiswa: RepositoriSiswa
+}
+
+class ContainerDataApp(private val context: Context): ContainerApp {
+    override val repositoriSiswa: RepositoriSiswa by lazy {
+        OfflineRepositoriSiswa(
+            siswaDao = DatabaseSiswa.getDatabase(context).siswaDao()
+        )
+    }
+}
+
+class AplikasiSiswa : Application() {
+    /**
+    AppContainer instance digunakan oleh kelas-kelas lainnya untuk mendapatkan dependensi
+     */
+
+    lateinit var container: ContainerApp
+
+    override fun onCreate() {
+        super.onCreate()
+        container = ContainerDataApp(context = this)
+    }
+}
