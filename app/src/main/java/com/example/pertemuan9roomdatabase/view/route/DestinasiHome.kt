0e65816e0faae1a30478f5d4fb1c36c99f499875
adilaroisa <adilaroisa688@gmail.com>
2025-11-28 13:41:05 +0700

    Membuat object navigasi DestinasiHome untuk halaman utama

---
+++ app/src/main/java/com/example/pertemuan9roomdatabase/view/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.pertemuan9roomdatabase.view.route
+
+import com.example.pertemuan9roomdatabase.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
