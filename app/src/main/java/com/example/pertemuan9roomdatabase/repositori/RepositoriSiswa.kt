b4bd4d55b5d7200eecc73f68aa25de746bcea370
adilaroisa <adilaroisa688@gmail.com>
2025-11-28 13:52:51 +0700

    Membuat interface RepositoriSiswa dan implementasi OfflineRepositoriSiswa untuk manajemen data

---
+++ app/src/main/java/com/example/pertemuan9roomdatabase/repositori/RepositoriSiswa.kt
@@ -0,0 +1,16 @@
+package com.example.pertemuan9roomdatabase.repositori
+
+import com.example.pertemuan9roomdatabase.room.Siswa
+import com.example.pertemuan9roomdatabase.room.SiswaDao
+import kotlinx.coroutines.flow.Flow
+
+
+interface RepositoriSiswa {
+    fun getAllSiswaStream(): Flow<List<Siswa>>
+    suspend fun insertSiswa(siswa: Siswa)
+}
+
+class OfflineRepositoriSiswa(private val siswaDao: SiswaDao) : RepositoriSiswa {
+    override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
+    override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
+}
