91b3990a4aa6e70baf1514e80d377005a0c11c4c
adilaroisa <adilaroisa688@gmail.com>
2025-11-28 13:40:09 +0700

    Membuat object navigasi DestinasiEntry untuk halaman tambah data

---
+++ app/src/main/java/com/example/pertemuan9roomdatabase/view/route/DestinasiEntry.kt
@@ -0,0 +1,9 @@
+package com.example.pertemuan9roomdatabase.view.route
+
+import com.example.pertemuan9roomdatabase.R
+import com.example.pertemuan9roomdatabase.view.route.DestinasiNavigasi
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
