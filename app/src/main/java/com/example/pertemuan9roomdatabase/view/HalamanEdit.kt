3a4db99bfb2345e4aa9f442bfdd0ca1cfdea1c6a
adilaroisa <adilaroisa688@gmail.com>
2025-12-05 13:05:08 +0700

    menambahkan halamanEdit

---
+++ app/src/main/java/com/example/pertemuan9roomdatabase/view/HalamanEdit.kt
@@ -0,0 +1,49 @@
+package com.example.myroomsiswa.view
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.pertemuan9roomdatabase.view.EntrySiswaBody
+import com.example.pertemuan9roomdatabase.view.SiswaTopAppBar
+import com.example.pertemuan9roomdatabase.view.route.DestinasiEditSiswa
+import com.example.pertemuan9roomdatabase.viewmodel.EditViewModel
+import com.example.pertemuan9roomdatabase.viewmodel.provider.PenyediaViewModel
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditSiswaScreen(
+    navigateBack: () -> Unit,
+    onNavigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    Scaffold(
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiEditSiswa.titleRes),
+                canNavigateBack = true,
+                navigateUp = onNavigateUp
+            )
+        },
+        modifier = modifier
+    ) { innerPadding ->
+        val coroutineScope = rememberCoroutineScope()
+        EntrySiswaBody(
+            uiStateSiswa = viewModel.uiState,
+            onSiswaValueChange = viewModel::updateUiState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.updateSiswa()
+                    navigateBack()
+                }
+            },
+            modifier = modifier.padding(innerPadding)
+        )
+    }
+}
