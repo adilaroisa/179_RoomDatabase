3a172e8a18088bcca64efe90c244a209047757c3
adilaroisa <adilaroisa688@gmail.com>
2025-12-05 13:04:45 +0700

    menambahkan fungsi delete, update, read dan override di repositorisiswa

--- app/src/main/java/com/example/pertemuan9roomdatabase/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/pertemuan9roomdatabase/repositori/RepositoriSiswa.kt
@@ -8,9 +8,18 @@ import kotlinx.coroutines.flow.Flow
 interface RepositoriSiswa {
     fun getAllSiswaStream(): Flow<List<Siswa>>
     suspend fun insertSiswa(siswa: Siswa)
+    //edit 1 : tambah 3 fungsi berikut
+    fun getSiswaStream(id: Int): Flow<Siswa?>
+    suspend fun deleteSiswa(siswa: Siswa)
+    suspend fun updateSiswa(siswa: Siswa)
 }
 
 class OfflineRepositoriSiswa(private val siswaDao: SiswaDao) : RepositoriSiswa {
     override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
     override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
+
+    //edit 2 : tambah 3 override berikut
+    override fun getSiswaStream(id: Int): Flow<Siswa?> = siswaDao.getSiswa(id)
+    override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
+    override suspend fun updateSiswa(siswa: Siswa) = siswaDao.update(siswa)
 }
